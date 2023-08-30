6f754c2b35bafb51cdac05cef2e1e2a8c83f2f7a
Marc Aradillas <aradillasm@gmail.com>
2023-08-30 14:52:20 -0500

    Added and modifeid files

--- wrangle.py
+++ wrangle.py
@@ -4,9 +4,9 @@ import numpy as np
 
  # Acquire data.
 # ----------------------ACQUIRE FUNCTION---------------------------------
-def get_z_data():
+def acquire_zillow():
     
-    filename = 'sfr_2017.csv'
+    filename = 'zillow_data.csv'
 
     if os.path.isfile(filename):
         
@@ -33,36 +33,41 @@ def get_z_data():
                 
         df = pd.read_sql(query, url)
 
-        df.to_csv(filename, index=False)
-
         return df 
 
 
 # Prep data.
 # ---------------------------CLEAN FUNCTION----------------------------
-def clean_z_data():
+def clean_zillow():
     
-    df = get_z_data()
+    df = acquire_zillow()
     
     # Drop rows with missing values in any column
     df = df.dropna()
 
+    #rename cols
+    df = df.rename(columns= {'bedroomcnt': 'bedrooms', 'bathroomcnt': 'bathrooms', 'calculatedfinishedsquarefeet' : 'area', 'taxvaluedollarcnt': 'tax_value', 'yearbuilt': 'year_built', 'taxamount': 'tax_amount'})
+
     # Convert selected columns to integer type
-    int_columns = ['fips', 'yearbuilt', 'taxvaluedollarcnt', 'calculatedfinishedsquarefeet']
+    int_columns = ['fips', 'year_built', 'tax_value', 'area', 'bedrooms']
     df[int_columns] = df[int_columns].astype(int)
 
     # Remove rows where bathroomcnt or bedroomcnt is 0.0
-    df = df[(df.bathroomcnt != 0.0) & (df.bedroomcnt != 0.0)]
+    # df = df[(df.bedrooms != 0.0) & (df.bathrooms != 0.0)]
 
     return df
-
 
 # wrapping Acquire and Prep functions into one.
 # ---------------------------- WRANGLE ZILLOW -------------------------------------
 def wrangle_zillow():
 
-    df = get_z_data()
+    # acquire data function
+    df = acquire_zillow()
+    
+    # clean data function
+    df = clean_zillow()
 
-    df = clean_z_data()
+    # save to csv
+    df.to_csv('zillow_data.csv',index=False)
 
     return df
